031dc925868c5271474666b748f0d91683476612
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-01-06 21:07:56 +0100

    add build variables

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,8 @@ export * from './abortAble';
 export * from './style';
 export * from './mixin';
 export * from './table';
+
+declare const __VERSION__: string;
+declare const __BUILD_ID__: string;
+export const version = __VERSION__;
+export const buildId = __BUILD_ID__;
