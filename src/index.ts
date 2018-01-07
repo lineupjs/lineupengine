5c153e6ace0966120526118bd06ca81d6b5d827c
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-01-07 02:19:01 +0100

    play with bundling

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,4 @@ export * from './mixin';
 export * from './table';
 
 declare const __VERSION__: string;
-declare const __BUILD_ID__: string;
 export const version = __VERSION__;
-export const buildId = __BUILD_ID__;
