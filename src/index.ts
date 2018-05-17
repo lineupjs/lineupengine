325c4f46eeda973f9f463169365fac546d589b49
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-05-17 15:13:10 +0200

    add missing abortAble export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './ACellRenderer';
 export * from './ARowRenderer';
 export * from './logic';
 export * from './abortAble';
+export {default as abortAble} from './abortAble';
 export * from './style';
 export * from './mixin';
 export * from './table';
