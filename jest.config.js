441ddc98f674fb5800abf629aaa2a3462f0187a0
Samuel Gratzl <sam@sgratzl.com>
2021-02-26 22:31:43 +0100

    build: simplify test setup

--- jest.config.js
+++ jest.config.js
@@ -3,15 +3,9 @@
 module.exports = {
   preset: 'ts-jest',
   testRegex: '/tests/.*\\.spec\\.(ts|tsx|js)$',
-  haste: {
-    throwOnModuleCollision: false,
-  },
-  modulePathIgnorePatterns: ['node_modules/terser', 'node_modules/jest-runner'],
   globals: {
     'ts-jest': {
-      tsConfig: 'tsconfig.test.json',
-      // diagnostics: true,
-      // babelConfig: false,
+      tsconfig: 'tsconfig.test.json',
     },
   },
 };
